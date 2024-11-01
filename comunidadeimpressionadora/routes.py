3f2a6eb7d93b7892f78154c4605be33146633297
NatanaelBalbino <natanael.m.m.balbino@gmail.com>
2024-11-01 06:15:26 -0300

    validação de Login Criada

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, url_for, request, flash
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta
 from comunidadeimpressionadora.models import Usuario
+from flask_login import login_user
 
 lista_usuarios = ['Natanael', 'Samuel', 'Renato', 'Joel']
 
@@ -27,10 +28,15 @@ def login():
     form_criarconta = FormCriarConta()
 
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        # fez login com sucesso
-        flash('Login Feito com sucesso', 'alert-info')
-        # redirecionando para outra tela
-        return redirect(url_for('home'))
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            # fez login com sucesso
+            flash('Login Feito com sucesso', 'alert-info')
+            # redirecionando para outra tela
+            return redirect(url_for('home'))
+        else:
+            flash('Falha no Login. Verifique Email e Senha', 'alert-danger')
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         # criptografando a senha
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
