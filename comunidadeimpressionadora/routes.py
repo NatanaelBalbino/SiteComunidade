bacf2608d0f20b8f29c9f1bc7e218429c8c5d4a5
NatanaelBalbino <natanael.m.m.balbino@gmail.com>
2024-11-09 11:42:26 -0300

    apresentando os posts na Home

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -10,7 +10,8 @@ import os
 
 @app.route("/")  # Decorators
 def home():
-    return render_template("home.html")
+    posts = Post.query.all()
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/contato")
