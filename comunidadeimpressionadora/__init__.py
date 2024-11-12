e0fd5eb71c5a14f5f77e9f517eb413418f256bbe
NatanaelBalbino <natanael.m.m.balbino@gmail.com>
2024-11-12 05:22:12 -0300

    ajustado codigo para criar DB

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import sqlalchemy
 import os
 
 app = Flask(__name__)
@@ -18,5 +19,16 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print('DataBase Criada com Sucesso')
+else:
+    print('Base de Dados já existente!')
 
 from comunidadeimpressionadora import routes
